3f9655b14210ab37112bacdeb5d152c66d68167e
Sander Nemvalts <snemvalts@gmail.com>
2014-02-27 15:00:03 +0200

    Initial-er commit

---
+++ index.js
@@ -0,0 +1,16 @@
+module.exports = readingTime
+function readingTime(callback,data,wpm){
+	if(typeof wpm !== "number") wpm = 250;
+	if (typeof data === "string"){
+		return callback(data.split(" ").length / wpm)
+	}
+	if(data instanceof Array){
+		return callback(data.length / wpm)
+	}
+	if(typeof data === "number"){
+		return callback(data/wpm)
+	}
+	else{
+		return callback(new Error("Provided data isn't one of the following: string, array or number"));
+	}
+}
---
+++ test.js
@@ -0,0 +1,6 @@
+var readingTime = require("./index.js");
+readingTime(function(data){
+	console.log(data+" minutes.")
+},["hello", "my", "name", "is", "noone","lol",
+"hello", "my", "name", "is", "noone","lol","hello", "my", "name", "is", "noone","lol",
+"hello", "my", "name", "is", "noone","lol","hello", "my", "name", "is", "noone","lol",])
